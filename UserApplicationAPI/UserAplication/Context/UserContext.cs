76638200021d4cf825ff23ea4adffaf42d80d995
Yuri <keenandigimon@gmail.com>
2023-01-26 19:32:39 -0300

    main commit

---
+++ UserApplicationAPI/UserAplication/Context/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAplication.Modals;
+
+namespace UserAplication.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server = .\\SQLEXPRESS; Database = CrudUser; Trusted_Connection = true; TrustServerCertificate = true");
+        }
+    }
+}
---
+++ UserApplicationAPI/UserAplication/Modals/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserAplication.Modals
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+
+
+}
